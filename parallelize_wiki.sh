59b37f4a85024c60ccabe6171e19a7cd96680e39
arielah <ariel.hippen@gmail.com>
2020-01-20 12:14:09 -0500

    Cleaning up main directory

--- parallelize_wiki.sh
+++ parallelize_wiki.sh
@@ -1,13 +1,9 @@
 mkdir tmp
 
-
-#split url list into 20 equal parts
+# Split url list into 20 equal parts
 split -n l/20 data/all_url_pages.tsv tmp/urls_
 
-#| parallel -j5 python3 02.scrape-wikipedia.py
-
+# Process url chunks in parallel, 3 at a time (more causes 'connection refused' issues)
 parallel --link -j3 python3 02.scrape-wikipedia.py --u {1} --o {2} ::: tmp/urls_a{a..t} ::: tmp/output_urls_a{a..t}
 
-#TODO: change to wait for all scripts to finish
-
-cat tmp/output_urls* > data/new_scraped_names.tsv
+cat tmp/output_urls* > data/scraped_names.tsv
